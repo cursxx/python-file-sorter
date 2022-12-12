b47af6eb56eaf19543c440611a4c652cf5743587
Deviant Mark <deviantmark@cursx.ru>
2022-12-13 00:39:19 +0700

    new fixes

--- src/actions.py
+++ src/actions.py
@@ -16,15 +16,15 @@ class FileActions():
         if os.path.exists(self.current_path):
             if os.path.exists(self.current_path):
                 self.askExists()
-                self.moveFile()
+                # self.moveFile()
                 print("% s перемещен в указанное место,% s" % (source_path , new_location))
             elif os.path.isfile(self.current_path):
                 self.askExists()
-                self.moveFile()
+                # self.moveFile()
                 print("% s перемещен в указанное место,% s" % (source_path , new_location))
             elif os.path.isdir(self.current_path):
                 self.askExists()
-                self.moveFile()
+                # self.moveFile()
                 print("% s перемещен в указанное место,% s" % (source_path , new_location))
         else:
             # Распечатать сообщение, если файл не существует
@@ -44,20 +44,17 @@ class FileActions():
             # Отобразит только файлы и папки в текущей директории
             break
     
-    # Функция перемещения файлов 
-    def moveFile(self):
-        new_location = shutil.move(source_path, destination_path)
-    
     # Проверка существования файлов с определенными расширениями и объявление переменных
     def askExists(self):
-        self.extension = ['pdf', 'doc', 'docx', 'jpg', 'jpeg', 'png', 'rar', 'zip', '7z', 'exe', 'txt', 'xlsx', 'xls']
-        extension_alone = np.array_split(self.extension, 13)
-        for fileNumber in self.extension:
-            if os.path.exists('^.{3,20}$\.', self.extension[fileNumber]):
-                if not os.path.exists(destination_path):
-                    os.makedirs(destination_path)
-                self.current_path = os.getcwd()
-                # Задайте имя файла с путем
-                self.source_path = "/home/markus/devops/personal/file-sorter/src/to_sort/^.{3,20}$\." + extension
-                # Задайте путь к каталогу, в который будет перемещен файл
-                self.destination_path = "/home/markus/devops/personal/file-sorter/src/sorted/" + extension
+        # self.extension = ['pdf', 'doc', 'docx', 'jpg', 'jpeg', 'png', 'rar', 'zip', '7z', 'exe', 'txt', 'xlsx', 'xls']
+        # extension_alone = np.array_split(self.extension, 13)
+        
+        if os.path.exists('^.{3,20}$\.pdf'):
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+            current_path = os.getcwd()
+            # Задайте имя файла с путем
+            source_path = "/home/markus/devops/personal/file-sorter/src/to_sort/^.{3,20}$\.pdf"
+            # Задайте путь к каталогу, в который будет перемещен файл
+            destination_path = "/home/markus/devops/personal/file-sorter/src/sorted/"
+            new_location = shutil.move(source_path, destination_path)
